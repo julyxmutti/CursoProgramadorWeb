b3c7c5117d24d813bc36ba26ce9b4a28407ad16f
Julieta Mutti Tovar <40809007+julyxmutti@users.noreply.github.com>
2019-05-08 21:46:54 -0300

    Add files via upload

---
+++ Clase 8/main.js
@@ -0,0 +1,31 @@
+// Bloque de codigo que se ejecuta cuando es llamado dentro de nuestro archivo
+// prompt()
+
+// Subdivirdir un problema en funciones
+// prompt ("titulo", "value")
+function mensaje(){
+    document.write("Holissss");
+    document.write("*************")
+}
+// Que necesita la funcion apra resolver el problema
+// Parametros
+// Un usuario ingresa a traves del prompt 3 numeros
+// Los parametros van separados por COMAS (no punto y coma)
+// Los parametros pueden tener cualquier nombre
+function sumar(num1, num2, num3){
+document.write("El resultado es: " + resultado);
+
+}
+function DividirNumero(num1,num2){
+// Las variales que se definen en el argumento de la funcion es provada. Es decir, fuera de las llaves () de la funcion no esta definida
+    if (num2 == 0){
+        document.write("No puedo dividir");
+
+    } else{
+        var resultado = num1/num2
+        document.write
+
+    }
+}function contador(){
+    alert("Me apretaste");
+}
